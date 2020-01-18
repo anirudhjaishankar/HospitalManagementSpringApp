7f4c04eb3fb7f59f4001e0ce34070aeadb07aadb
Anirudh <anirudhjaishankar97@gmail.com>
2020-01-18 23:12:02 +0530

    Patient CRUD Working

--- src/main/java/global/coda/hms/controller/PatientController.java
+++ src/main/java/global/coda/hms/controller/PatientController.java
@@ -1,7 +1,9 @@
 package global.coda.hms.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,5 +34,16 @@ public class PatientController {
 		return patientService.insertPatient(patient);
 	}
 	
+	@PutMapping("/update/{id}")
+	@ResponseBody
+	int updatePatient(@RequestBody PatientBean newPatient, @PathVariable("id") int id ) {
+	    return patientService.updatePatient(newPatient, id);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	@ResponseBody
+	int deletePatient(@PathVariable("id") int id) {
+	    return patientService.deletePatient(id);
+	}
 
 }
--- src/main/java/global/coda/hms/service/PatientService.java
+++ src/main/java/global/coda/hms/service/PatientService.java
@@ -1,6 +1,5 @@
 package global.coda.hms.service;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,26 +11,38 @@ import global.coda.hms.beans.PatientBean;
 
 @Service
 public class PatientService {
-	
-	@Autowired
-	PatientMapper patientMapper;
-	
-	@Autowired 
-	AddressMapper addressMapper;
-	
-	@Autowired
-	UserMapper userMapper;
-	
 
+    @Autowired
+    PatientMapper patientMapper;
 
-	public PatientBean getPatientById(int id) {
-		return patientMapper.getPatientById(id);
-	}
-	
-	public int insertPatient(PatientBean newPatient) {
-		AddressBean address = newPatient.getAddress();
-		int addressRowsAffected = addressMapper.insertAddress(address);
-		int userRowsAffected = userMapper.insertUser(newPatient);
-		return patientMapper.insertPatients(newPatient);
-	}
+    @Autowired
+    AddressMapper addressMapper;
+
+    @Autowired
+    UserMapper userMapper;
+
+    public PatientBean getPatientById(int id) {
+        return patientMapper.getPatientById(id);
+    }
+
+    public int insertPatient(PatientBean newPatient) {
+        AddressBean address = newPatient.getAddress();
+        int addressRowsAffected = addressMapper.insertAddress(address);
+        int userRowsAffected = userMapper.insertUser(newPatient);
+        return patientMapper.insertPatient(newPatient);
+    }
+
+    public int updatePatient(PatientBean newPatient, int patientId) {
+        AddressBean address = newPatient.getAddress();
+        int patientRowsAffected = patientMapper.updatePatient(newPatient, patientId);
+        int userId = patientMapper.getUserIdFromPatient(patientId);
+        userMapper.updateUser(newPatient, userId);
+        int addressId = userMapper.getAddressIdFromUser(userId);
+        int addressRowsAffected = addressMapper.updateAddress(address, addressId);
+        return 0;
+    }
+    
+    public int deletePatient(int patientId) {
+        return patientMapper.deletePatient(patientId);
+    }
 }
